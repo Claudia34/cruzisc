202b39430621e14635ca2e7e266c2e549bbdf07a
clau95 <clau95kari@gmail.com>
2018-03-12 18:44:23 -0600

    se agrega forms a tabla a clientes

---
+++ kati20/clientesForm2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kati20
+{
+    public partial class clientesForm2 : Form
+    {
+        public clientesForm2()
+        {
+            InitializeComponent();
+        }
+
+        private void clientesBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.clientesBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.basedatos1DataSet);
+
+        }
+
+        private void clientesForm2_Load(object sender, EventArgs e)
+        {
+            // TODO: esta línea de código carga datos en la tabla 'basedatos1DataSet.clientes' Puede moverla o quitarla según sea necesario.
+            this.clientesTableAdapter.Fill(this.basedatos1DataSet.clientes);
+
+        }
+    }
+}
