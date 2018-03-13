288a183395f5ce12375401ae186ece7ec31f0868
clau95 <clau95kari@gmail.com>
2018-03-12 19:04:28 -0600

    se agraga froms  a tabla proveedores

---
+++ kati20/proveedoresForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kati20
+{
+    public partial class proveedoresForm : Form
+    {
+        public proveedoresForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
